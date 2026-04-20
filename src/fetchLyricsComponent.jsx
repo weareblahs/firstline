65edac8d330861c962d8dae28c98ca54ecc891ca
nottanyuxuan <tanyuxuan2005@gmail.com>
2026-04-21 02:32:33 +0800

    clear readme for future edits / fix fetchLyricsComponent

--- src/fetchLyricsComponent.jsx
+++ src/fetchLyricsComponent.jsx
@@ -10,17 +10,35 @@ export const fetchLyrics = async (songName) => {
       const preprocessData = data.data.find(
         (s) => s.title && s.title.toLowerCase() === songName.toLowerCase(),
       );
+      const info = ["", ""];
 
-      // special case: if the artist name contains "," and "&" replace it appropriately
-      //               so that the Lyrics.ovh API can parse it properly
+      // if the artist name contains "," and "&" replace it appropriately
+      // so that the Lyrics.ovh API can parse it properly
       // example: "Earth, Wind & Fire" => "Earth Wind and Fire"
+
+      // if the found song name contains multiple parentheses, it removes everything from second parantheses onwards
+      // example: "I Want You (She's So Heavy) (Remastered 2009)" => "I Want You (She's So Heavy)"
 
       if (preprocessData) {
         // found lyrics with exact name as song
-        constructed = `${data.data[0].artist.name.replace(",", "").replace("&", "and")}/${preprocessData.title.replace(",", "").replace("&", "and").replace("/", " ").replace("'", "")}}`;
+        constructed = `${preprocessData.artist.name.replace(",", "").replace("&", "and")}/${preprocessData.title
+          .replace(",", "")
+          .replace("&", "and")
+          .replace("/", " ")
+          .replace("'", "")
+          .replace(/(\(.*?\).*?)\(.*?\).*/, "$1")}}`;
+        info[0] = preprocessData.artist.name;
+        info[1] = preprocessData.title;
       } else {
         // else get first from index
-        constructed = `${data.data[0].artist.name.replace(",", "").replace("&", "and")}/${data.data[0].title.replace(",", "").replace("&", "and").replace("/", " ").replace("'", "")}`;
+        constructed = `${data.data[0].artist.name.replace(",", "").replace("&", "and")}/${data.data[0].title
+          .replace(",", "")
+          .replace("&", "and")
+          .replace("/", " ")
+          .replace("'", "")
+          .replace(/(\(.*?\).*?)\(.*?\).*/, "$1")}`;
+        info[0] = data.data[0].artist.name;
+        info[1] = data.data[0].title;
       }
       // construct URL
       const constructedData = await ky
@@ -29,7 +47,7 @@ export const fetchLyrics = async (songName) => {
       if (constructedData) {
         // return lyrics in plaintext
         return {
-          songInfo: constructed.split("/"),
+          songInfo: info,
           lyrics: constructedData.lyrics,
         };
       }
